f740a4ae6a378f83e21c16f5e1a9d907c9fb4019
Jonah Hess <jonahhess@outlook.com>
2023-07-30 22:22:22 +0300

    Implemented Collision Detection

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 # Constants for the game window size and colors
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+ELEMENT_WIDTH, ELEMENT_HEIGHT = 30,30
 BACKGROUND_COLOR = (0, 0, 0)
 
 def main():
@@ -18,11 +19,10 @@ def main():
     # Desired width and height for Pacman and Ghost sprites
     pacman_width, pacman_height = 30, 30
     ghost_width, ghost_height = 30, 30
-    background_width, background_height = 800,600
 
-    pacman_image = pygame.transform.scale(pacman_image, (pacman_width, pacman_height))
-    ghost_image = pygame.transform.scale(ghost_image, (ghost_width, ghost_height))
-    background_image = pygame.transform.scale(background_image, (background_width, background_height))
+    pacman_image = pygame.transform.scale(pacman_image, (ELEMENT_WIDTH, ELEMENT_HEIGHT))
+    ghost_image = pygame.transform.scale(ghost_image, (ELEMENT_WIDTH, ELEMENT_HEIGHT))
+    background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
     pacman_x, pacman_y = SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2
     ghost_x, ghost_y = SCREEN_WIDTH // 4, SCREEN_HEIGHT // 4
@@ -62,7 +62,7 @@ def main():
             elif pacman_direction == "RIGHT":
                 pacman_x += pacman_speed
                     
-        
+        collision_detection(screen,pacman_x,pacman_y,ghost_x,ghost_y)
 
         # Draw Pacman, Ghosts, pellets, walls, and other game elements here
         pygame.display.flip()
@@ -70,5 +70,28 @@ def main():
         # Set the frame rate
         clock.tick(60)
 
+
+def collision_detection(screen,a_x,a_y,b_x,b_y):
+    x_difference = abs(a_x - b_x)
+    y_difference = abs(a_y - b_y)
+    #print(x_difference,y_difference)
+
+    if x_difference < 20 and y_difference < 20:
+        font = pygame.font.SysFont('arial', 36)  # None means default font, 36 is the font size
+        message = "Collision Detected!"
+
+        # Render the message on the screen
+        text_surface = font.render(message, True, (0, 0, 0))  # (0, 0, 0) is black color
+        text_rect = text_surface.get_rect()
+        text_rect.center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+        # Blit the text surface onto the screen
+        screen.blit(text_surface, text_rect)
+        return
+    else:
+        return
+
+
 if __name__ == "__main__":
     main()
+
